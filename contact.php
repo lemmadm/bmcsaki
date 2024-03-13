45e96e565c76d9e8ee9eeda0745769fecc8c14b1
LemmaIoT <49133272+lemmadm@users.noreply.github.com>
2024-03-13 10:46:49 +0100

    Initial Upload

---
+++ contact.php
@@ -0,0 +1,44 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the form fields and remove whitespace
+    $name = strip_tags(trim($_POST["name"]));
+    $name = str_replace(array("\r","\n"),array(" "," "),$name);
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $phone = trim($_POST["phone"]);
+    $message = trim($_POST["message"]);
+
+    // Check if all fields are filled
+    if (empty($name) || empty($email) || empty($phone) || empty($message)) {
+        http_response_code(400);
+        echo "Please fill out all fields.";
+        exit;
+    }
+
+    // Set the recipient email address
+    $recipient = "bmcsaki@yahoo.com";
+
+    // Set the email subject
+    $subject = "New Form Submission from $name";
+
+    // Build the email content
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n";
+    $email_content .= "Phone: $phone\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Build the email headers
+    $email_headers = "From: $name <$email>";
+
+    // Send the email
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        http_response_code(200);
+        echo "Thank You! Your message has been sent.";
+    } else {
+        http_response_code(500);
+        echo "Oops! Something went wrong and we couldn't send your message.";
+    }
+} else {
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
+?>
